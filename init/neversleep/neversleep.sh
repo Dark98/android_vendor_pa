9838077fa43f3bde90a2192dfd2b41089045a971
Felix <none@none.none>
2019-01-09 21:50:20 +0100

    Fix neversleep

--- init/neversleep/neversleep.sh
+++ init/neversleep/neversleep.sh
@@ -3,8 +3,7 @@
 # HACK: Little helper script to set default neversleep option
 
 _neversleep=$(getprop persist.vendor.neversleep)
-if [ $_neversleep == "" ]
-then
+if [ "$_neversleep" = "" ]; then
     setprop persist.vendor.neversleep 1
+    log -t "neversleep" -p w "HACK: Disabling sleep. Set persist.vendor.neversleep to 1 to re-enable deep sleep"
 fi
-
