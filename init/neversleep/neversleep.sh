8d881673ab909dc6ec6ed298a71a7a8fc729ac1e
Felix <none@none.none>
2019-01-09 22:29:31 +0100

    Logging for neversleep

--- init/neversleep/neversleep.sh
+++ init/neversleep/neversleep.sh
@@ -3,7 +3,12 @@
 # HACK: Little helper script to set default neversleep option
 
 _neversleep=$(getprop persist.vendor.neversleep)
-if [ "$_neversleep" = "" ]; then
+if [ $_neversleep = "1" ]; then
+    log -t "neversleep" -p w "HACK: Disabling deep sleep. Set persist.vendor.neversleep to 0 to re-enable."
+elif [ $_neversleep = "0" ]; then
+    log -t "neversleep" -p w "Deep sleep enabled. Set persist.vendor.neversleep to 1 to disable deep sleep"
+else
     setprop persist.vendor.neversleep 1
     log -t "neversleep" -p w "HACK: Setting persist.vendor.neversleep to 1"
+    log -t "neversleep" -p w "HACK: Disabling deep sleep. Set persist.vendor.neversleep to 0 to re-enable"
 fi
